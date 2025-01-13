52f509e9fbddcf63796f1da776b9044cb2066103
Luke Heuser <103976088+LukeHeuser@users.noreply.github.com>
2025-01-12 22:12:08 -0500

    Fourth commit

--- src/Main.java
+++ src/Main.java
@@ -24,7 +24,9 @@ public class Main {
 
 //        gettingElements(placesToVisit);
 
-        printItinerary3(placesToVisit);
+//        printItinerary3(placesToVisit);
+
+        testIterator(placesToVisit);
 
 
     }
@@ -134,5 +136,27 @@ public class Main {
 
     }
 
+    private static void testIterator(LinkedList<String> list) {
+
+        var iterator = list.listIterator();
+
+        while(iterator.hasNext()) {
+
+            if(iterator.next().equals("Brisbane")) {
+                iterator.add("Lake Wivenhoe");
+            }
+        }
+
+        while(iterator.hasPrevious()) {
+            System.out.println(iterator.previous());
+        }
+        System.out.println(list);
+
+        var iterator2 = list.listIterator(3);
+        System.out.println(iterator2.previous());
+
+
+
+    }
 
 }
